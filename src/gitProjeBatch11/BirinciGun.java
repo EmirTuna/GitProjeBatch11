ec84dd8ce4bd12bcae531cd1da46876fcda892cb
isim <mailadresi>
2020-11-17 00:01:30 +0300

    Ikinci commit

--- src/gitProjeBatch11/BirinciGun.java
+++ src/gitProjeBatch11/BirinciGun.java
@@ -3,7 +3,9 @@ package gitProjeBatch11;
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("11:57 - Birinci Gun");
+		System.out.println("Ikinci commit");
 
 	}
 
